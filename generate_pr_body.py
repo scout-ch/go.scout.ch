8e751fde93d9730d8387f4d89904f4595541eb08
Diego Steiner <diego.steiner@u041.ch>
2026-04-18 13:47:44 +0200

    feat: fix

--- generate_pr_body.py
+++ generate_pr_body.py
@@ -1,20 +1,10 @@
-import io
 import os
-
-import qrcode
 
 entry_path = os.environ["ENTRY_PATH"]
 entry_url = os.environ["ENTRY_URL"]
 issue_number = os.environ["ISSUE_NUMBER"]
 final_url = f"https://go.scout.ch/{entry_path}"
-
-qr = qrcode.QRCode(border=1)
-qr.add_data(final_url)
-qr.make(fit=True)
-
-buffer = io.StringIO()
-qr.print_ascii(out=buffer)
-ascii_qr = buffer.getvalue()
+qr_url = f"https://api.qrserver.com/v1/create-qr-code/?size=200x200&data={final_url}"
 
 print(f"""\
 Adds redirect:
@@ -23,8 +13,7 @@ Adds redirect:
 - **Target**: {entry_url}
 - **Short URL**: {final_url}
 
-```
-{ascii_qr}```
+![QR Code]({qr_url})
 
 Closes #{issue_number}
 """)
